4644374049c5e4b7a0aad4875d056467af836f5b
Benjamin Weigel <bweigel@ipb-halle.de>
2015-06-11 12:03:56 +0200

    Graphics in PFOMT section, some results (variants, xtallization)

--- phd_thesis_WEB.R
+++ phd_thesis_WEB.R
@@ -1,15 +1,18 @@
+
 ## ----glossary_definitions, child='usr/subdocuments/options/open_science_glossary.Rnw', eval=T----
+
 
 
 
 ## ----chunk_global_r_options, child='usr/subdocuments/options/ost_global_r_options.Rnw', eval=T----
 
+
 ## ----ost_global_r_options, tidy=FALSE, highlight=FALSE, include=FALSE, cache=FALSE, eval=TRUE----
   ## Load packages 
   suppressPackageStartupMessages(library(ggplot2)) 
   suppressPackageStartupMessages(library(magrittr))
-  suppressPackageStartupMessages(library(dplyr)) 
   suppressPackageStartupMessages(library(plyr))
+  suppressPackageStartupMessages(library(dplyr)) 
   suppressPackageStartupMessages(library(rgbif))
   suppressPackageStartupMessages(library(gridExtra))
 
@@ -20,7 +23,7 @@
   # code evaluation 
   opts_chunk$set(eval=TRUE)
   # code decoration
-  opts_chunk$set(tidy=TRUE, prompt=TRUE, comment='##', highlight=TRUE, size='normalsize')
+  opts_chunk$set(tidy=TRUE, prompt=TRUE, comment='##', highlight=TRUE, size='scriptsize')
   # cache options
   opts_chunk$set(cache.path='usr/cache/')  
   # Caching on by default
@@ -102,11 +105,15 @@
 
 
 
+
+
 ## ----subdoc_content_scripts, eval=T, echo=F, cache=F---------------------
 read_chunk("usr/statistics/rproject/test.R")
 read_chunk("usr/statistics/rproject/litSAM.R")
 
+
 ## ----subdoc_content_header, child='usr/subdocuments/chapters/x_header.Rnw', eval=T----
+
 
 
 
@@ -114,11 +121,14 @@ read_chunk("usr/statistics/rproject/litSAM.R")
 
 
 
+
 ## ----subdoc_content_introduction, child='usr/subdocuments/chapters/b_introduction_section.Rnw', eval=T----
 
 
 
+
 ## ----subdoc_content_material_methods, child='usr/subdocuments/chapters/c_material_methods_section.Rnw', eval=T----
+
 
 ## ----test, echo=F, tidy=TRUE, fig.height=3.25, fig.width=5.25, dev='tikz'----
 load("../../statistics/data/WEB338.fa-ca.calibration.Rda")
@@ -128,11 +138,15 @@ ggplot(cali.curve, aes(x=FA, y=value, shape=as.factor(pH), color=as.factor(DES),
  theme_ost()
 
 
+
+
 ## ----subdoc_content_results, child='usr/subdocuments/chapters/d_pfomt_section.Rnw', eval=T----
 
-## ----litSAM, echo=F, tidy=TRUE, fig.height=3.25, fig.width=5.25, dev='tikz'----
+
+## ----litSAM, echo=F, tidy=TRUE, fig.height=5, fig.width=5, out.width='0.7\\textwidth', dev='pdf', error=T----
 load("../../statistics/data/litSAMder.Rda")
 
+#head(daf)
 a <- daf %>% group_by(substrate, target.mol, name, type) %>%
   summarise(T = sum(T), C = sum(C)) %>%
   ungroup %>% 
@@ -143,35 +157,37 @@ a <- daf %>% group_by(substrate, target.mol, name, type) %>%
         axis.text = element_blank(),
         axis.title = element_blank(),
         axis.ticks = element_blank(),
-        legend.position = "bottom") +
+        legend.position = "none") +
   geom_rect(data=nodes, aes(xmin=gpmin, xmax = gpmax, ymin=0, ymax=size), fill="black", color="white", alpha=0.2) +
   geom_bar(stat="identity", aes(x = substrate, fill=target.mol, y=T))  +
   geom_crossbar(data=tmp, aes(x=substrate, ymin=C, ymax=C, y=C), width=0.8, color="black") +
   geom_text(data=nodes, aes(x = mid, y=25, label=type, angle=angles)) 
 a <- a + coord_polar() 
 
-b <- daf %>% group_by(substrate, target.atom, name, type) %>%
-  summarise(T = sum(T), C = sum(C)) %>%
-  ungroup %>% 
-  ggplot(data=.) + 
-  geom_bar(stat="identity", aes(x = substrate, fill=target.atom, y=T)) +
-  scale_y_continuous(expand=c(0.2, 0))  +
-  theme(panel.background = element_blank(), 
-        axis.text = element_blank(),
-        axis.title = element_blank(),
-        axis.ticks = element_blank(),
-        legend.position = "bottom") +
-  geom_rect(data=nodes, aes(xmin=gpmin, xmax = gpmax, ymin=0, ymax=size), fill="black", color="white", alpha=0.2) +
-  geom_bar(stat="identity", aes(x = substrate, fill=target.atom, y=T))  +
-  geom_crossbar(data=tmp, aes(x=substrate, ymin=C, ymax=C, y=C), width=0.8, color="black") +
-  # scale_fill_manual(values =cols) +
-  geom_text(data=nodes, aes(x = mid, y=25, label=type, angle=angles)) 
-b <- b + coord_polar() 
+# b <- daf %>% group_by(substrate, target.atom, name, type) %>%
+#   summarise(T = sum(T), C = sum(C)) %>%
+#   ungroup %>% 
+#   ggplot(data=.) + 
+#   geom_bar(stat="identity", aes(x = substrate, fill=target.atom, y=T)) +
+#   scale_y_continuous(expand=c(0.2, 0))  +
+#   theme(panel.background = element_blank(), 
+#         axis.text = element_blank(),
+#         axis.title = element_blank(),
+#         axis.ticks = element_blank(),
+#         legend.position = "none") +
+#   geom_rect(data=nodes, aes(xmin=gpmin, xmax = gpmax, ymin=0, ymax=size), fill="black", color="white", alpha=0.2) +
+#   geom_bar(stat="identity", aes(x = substrate, fill=target.atom, y=T))  +
+#   geom_crossbar(data=tmp, aes(x=substrate, ymin=C, ymax=C, y=C), width=0.8, color="black") +
+#   geom_text(data=nodes, aes(x = mid, y=25, label=type, angle=angles)) 
+# b <- b + coord_polar() 
+a
+#grid.arrange(a,b, nrow=1)
 
-grid.arrange(a,b, nrow=1)
+
 
 
 ## ----subdoc_content_results, child='usr/subdocuments/chapters/e_noncatechol_section.Rnw', eval=T----
+
 
 
 
@@ -179,7 +195,9 @@ grid.arrange(a,b, nrow=1)
 
 
 
+
 ## ----subdoc_content_results, child='usr/subdocuments/chapters/g_des_section.Rnw', eval=T----
+
 
 
 
@@ -187,7 +205,9 @@ grid.arrange(a,b, nrow=1)
 
 
 
+
 ## ----subdoc_content_appendix, child='usr/subdocuments/chapters/x_appendix_section.Rnw', eval=T----
+
 
 ## ----test_plot_one_appendix, fig.width=3.415, fig.height=2.5, echo=FALSE----
    ggplot(movies, aes(x=rating)) +
@@ -195,7 +215,10 @@ grid.arrange(a,b, nrow=1)
    theme_ost() 
 
 
+
+
 ## ----subdoc_content_introduction, child='usr/subdocuments/chapters/x_affidavit_section.Rnw', eval=T----
+
 
 
 
@@ -203,7 +226,9 @@ grid.arrange(a,b, nrow=1)
 
 
 
+
 ## ----body_mainmatter_chapter_one, child='usr/subdocuments/chapters/x_glossaries_section.Rnw', eval=T----
+
 
 
 
